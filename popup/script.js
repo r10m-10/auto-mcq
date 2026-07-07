9237716405d93fba47c27da59acdcb8736b6bcde
R10M <reyaanshsharma11@gmail.com>
2026-07-07 17:20:19 +0530

    status & UI styling

--- popup/script.js
+++ popup/script.js
@@ -1,11 +1,41 @@
-async function getTabs() {
-    console.log("Start");
-    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
-    console.log("Done");
-}
+const status = document.querySelector(".cur-status")
 
-document.addEventListener('click', function (event) {
+document.addEventListener("DOMContentLoaded", async function () {
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
+    chrome.tabs.sendMessage(tabs[0].id, { action: "get-state" })
+    const response = await chrome.tabs.sendMessage(tabs[0].id, { action: "get-state" })
+    const btn = document.querySelector(`.circle-btn[data-option="${response.opt}"]`)
+    btn.classList.add("selected")
+    console.log(response.stat)
+    status.textContent = response.stat
+
+})
+
+document.addEventListener('click', async function (event) {
     const button = event.target.closest(".circle-btn")
-    const selectedOption = {"option": button.dataset.option}
-    console.log(selectedOption)
-});
+    if (!button) {
+        return
+    }
+    document.querySelectorAll(".circle-btn").forEach(btn => {
+        btn.classList.remove("selected")
+    })
+    button.classList.add("selected")
+    curStat = 
+    status.textContent = `CLICKING ${button.dataset.option}...`
+    const selectedOption = { action: "set-option", "option": button.dataset.option }
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true })
+    chrome.tabs.sendMessage(tabs[0].id, selectedOption)
+})
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    const clicked = message.clicked
+    const opt = message.option
+    if (clicked) {
+        const btn = document.querySelector(`.circle-btn[data-option="${opt}"]`)
+        btn.classList.remove("selected")
+        status.textContent = "CLICKED!"
+        setTimeout(() => {
+        }, 5000)
+        status.textContent = "ACTIVE"
+    }
+})
--- ved.js
+++ ved.js
@@ -1,16 +1,31 @@
-function checkForQuiz() {
-console.log("Mutation callback")
-const selectedElement = document.querySelector(`[data-option="${selection}"]`)
-if (selectedElement) {
-    selectedElement.click()
-    console.log("Clicking");
-    observer.disconnect()
-}}
+let selection = null
+let stat = "ACTIVE"
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    if (message.action == "get-state"){
+        sendResponse({opt: selection, stat: stat})
+    }
+    else if (message.action == "set-option"){
+        selection = message.option
+        stat = `CLICKING ${selection}...`
+    }
+})
+
+async function checkForQuiz() {
+    console.log("Checking for quiz...")
+    const selectedElement = document.querySelector(`[data-option="${selection}"]`)
+    if (selectedElement) {
+        selectedElement.click()
+        selection = null
+        chrome.runtime.sendMessage({option: selection, clicked: true})
+    }
+}
 
 const observer = new MutationObserver(checkForQuiz)
 
-observer.observe(document.body, {
-    childList: true,
-    subtree: true
+document.addEventListener("DOMContentLoaded", function () {
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    })
 })
-console.log(document.body)
